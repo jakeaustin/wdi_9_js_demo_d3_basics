5d0553b73757659a7e32556028065ba9b67d8d18
Jake Austin <jakeaust07@gmail.com>
2014-08-05 17:24:51 -0400

    bar graph avg salary by title by department
    weird bar interactions that no one can explain

--- demo.js
+++ demo.js
@@ -1,2 +1,99 @@
 // Writing demo code in here
-d3.select('#simple-list').append('li').text('It works!');
+//d3.select('#simple-list').append('li').text('It works!');
+//d3.selectAll('#simple-list li').style('color', 'tomato');
+  //better written as
+//d3.select('#simple-list').selectAll('li').style('color', 'tomato');
+
+//d3.select('#simple-form').attr()
+//d3.selectAll('#simple-list li').classed('tomato', true);
+// false to 'remove' class
+
+var groceryList = ['cheese', 'crackers', 'hummus'];
+var newList = ['things', 'stuff', 'other stuff', 'yep', 'cheese'];
+
+function updateGroceries(data) {
+  var list = d3.select('#simple-list')
+    .selectAll('li')
+      .data(data, function(d){ return d; })
+      .style('color', 'cyan');
+
+  list.enter().append('li')
+    .style('color', 'green')
+    .text(function(d, index){ return 'Index: ' + index + ' ' + d + '!'; });
+
+  list.exit().style('color', 'red');
+}
+
+//updateGroceries(groceryList);
+//updateGroceries(newList);
+
+// var friends = [
+//   { name: 'Chris', hair: 'brown' },
+//   { name: 'Jason', hair: 'red' },
+//   { name: 'mark', hair: 'blue' }
+// ];
+
+// d3.select('#simple-list')
+//   .selectAll('li')
+//     .data(friends)
+//   .enter().append('li')
+//     .style('color', function(d){ return d.hair; })
+//     .text(function(d){ return d.name + " has " + d.hair + ' hair'; });
+
+
+
+
+// var data = [4, 30, 20, 10, 5, 6, 200, 150, 99, 110, 3500];
+// var xScale = d3.scale.linear()
+//   .domain([0, d3.max(data)])
+//   .range([0, 800]);
+
+// d3.select('#html-chart')
+//   .selectAll('div')
+//     .data(data)
+//   .enter().append('div')
+//     .style('width', function(d){ return xScale(d) + 'px'; })
+//     .text(function(d){return d; });
+
+
+// var enteringGroup = d3.select('#svg-chart')
+//   .selectAll('g')
+//     .data(data)
+//   .enter().append('g');
+
+// enteringGroup.append('rect')
+//   .attr('height', 24)
+//   .attr('fill', 'steelblue')
+//   .attr('y', function(d, index){ return index*25;} )
+//   .attr('width', 0)
+//   .transition()
+//     .duration(1000)
+//       .attr('width', function(d) { return xScale(d); });
+
+
+// enteringGroup.append('text')
+//   .text(function(d){ return d; })
+//   .attr('x', 5)
+//   .attr('y', function(d, index){ return index*25 + 20; })
+//   .attr('fill', 'red');
+
+//source could be http://boston.gov/something/data/databits
+d3.json("sample.json", function(error, people) {
+  if(error){ alert('womp'); }
+
+  var chartHeight = 300;
+
+  var yScale = d3.scale.linear()
+    .domain([0, d3.max(people, function(d){ return d.yearsExperience; })])
+    .range([0, chartHeight]);
+
+  d3.select("#svg-chart")
+    .selectAll("rect")
+      .data(people)
+    .enter().append("rect")
+      .attr('fill', 'steelblue')
+      .attr('width', 40)
+      .attr('height', function(person) { return yScale(person.yearsExperience); })
+      .attr('x', function(person, index) { return index * 50; })
+      .attr('y', function(person) { return chartHeight - yScale(person.yearsExperience); });
+});
---
+++ svg-salaries.js
@@ -0,0 +1,100 @@
+var margin = { top: 50, right: 30, bottom: 250, left: 100 };
+var width = 1200 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+
+
+var xScale = d3.scale.ordinal()
+  .rangeRoundBands([0, width], 0.2);
+
+var yScale = d3.scale.linear()
+  .range([0, height]);
+
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+var containerGroup = d3.select("#svg-chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json('http://data.cityofboston.gov/resource/x8in-twjt.json', function(error, data) {
+  if(error){ alert('womp'); }
+
+
+  var depts = d3.set((data.map(function(d){ return d.department_name; }))).values();
+
+  d3.select('#dept-selector')
+    .selectAll('option')
+      .data(depts)
+    .enter().append('option')
+      .attr('value', function(d){ return d; })
+      .text(function(d){ return d; });
+  // listener (other d3 json for making the graph)
+
+    d3.select('#dept-selector').on('change', buildGraph);
+
+});
+
+function buildGraph() {
+
+  // use $this to get to selected dept
+  var dept = this.options[this.selectedIndex].value;
+  var queryString = 'http://data.cityofboston.gov/resource/x8in-twjt.json' + '?' + 'department_name='+dept;
+  d3.json(queryString, function(error, data) {
+    if(error){ alert('womp'); }
+
+    var in_dept = data.filter(function(d){
+      if (d.department_name === dept) {
+        return d;
+      }
+    });
+
+    var titles = d3.set(in_dept.map(function(d){ return d.title; })).values();
+    var salaries = titles.map(function(d){
+      var salary_in_pos = [];
+      in_dept.forEach(function(elem, index, array) {
+        if (d === elem.title) {
+          salary_in_pos.push(elem.regular);
+        }
+      });
+      var total = salary_in_pos.reduce(function(pv, cv) { return parseInt(pv) + parseInt(cv); }, 0);
+      return total / salary_in_pos.length;
+    });
+
+    xScale.domain(in_dept.map(function(d) { return d.title; }));
+    yScale.domain([d3.max(salaries), 0]);
+    d3.selectAll('.axis').remove();
+
+    containerGroup.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", "-.55em")
+        .attr("transform", "rotate(-90)" );
+
+    containerGroup.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+    var bars = containerGroup
+      .selectAll(".bar")
+        .data(in_dept);
+
+    bars.enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return xScale(d.title); })
+      .attr("y", function(d) { return yScale(d.regular); })
+      .attr("height", function(d) { return height - yScale(d.regular); })
+      .attr("width", xScale.rangeBand());
+    bars.exit().remove();
+  });
+}
